a7e982c38af65795c1063f6dce3f9f7e1fd192dc
sandipandandapat <sandipandandapat99@gmail.com>
2025-08-16 20:30:52 +0530

    Added feign client for inventory service

---
+++ product-service/src/main/java/com/ecommerce/controller/InventoryServiceClient.java
@@ -0,0 +1,14 @@
+package com.ecommerce.controller;
+
+import com.ecommerce.dto.InventoryRequestDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name = "inventory-service")
+public interface InventoryServiceClient {
+    @PostMapping("inventory/add")
+    public void addStock(@RequestBody List<InventoryRequestDto> request);
+}
---
+++ product-service/src/main/java/com/ecommerce/dto/InventoryRequestDto.java
@@ -0,0 +1,15 @@
+package com.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryRequestDto {
+    private String productId;
+    private int quantity;
+}
